2f4a38362589c850eb6a06ee93e98744d79b1be4
CGoddard1 <goddard.cole@gmail.com>
2023-03-07 15:39:12 -0500

    local storage array + new var/const

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,33 +17,47 @@
 
 
 
-var input = document.querySelector('.input_text');
-var main = document.querySelector('#name');
-var temp = document.querySelector('.temp');
-var desc = document.querySelector('.desc');
-var clouds = document.querySelector('.clouds');
-var button= document.querySelector('.submit');
+// var input = document.querySelector('.input_text');
+// var main = document.querySelector('#name');
+// var temp = document.querySelector('.temp');
+// var desc = document.querySelector('.desc');
+// var clouds = document.querySelector('.clouds');
+// var button= document.querySelector('.submit');
 
 
-button.addEventListener('click', function(name){
-// fetch('api.openweathermap.org/data/2.5/forecast?lat=40.7143&lon=74.006&appid=63606d5f87fb1ca9e2705345f9cb648e')
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=63606d5f87fb1ca9e2705345f9cb648e')
-// fetch('api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid=63606d5f87fb1ca9e2705345f9cb648e')
-.then(response => response.json())
-.then(data => {
-    console.log(data);
-  var tempValue = data['main']['temp'];
-  var nameValue = data['name'];
-  var descValue = data['weather'][0]['description'];
+// button.addEventListener('click', function(name){
+// // fetch('api.openweathermap.org/data/2.5/forecast?lat=40.7143&lon=74.006&appid=63606d5f87fb1ca9e2705345f9cb648e')
+// fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=63606d5f87fb1ca9e2705345f9cb648e')
+// // fetch('api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml&appid=63606d5f87fb1ca9e2705345f9cb648e')
+// .then(response => response.json())
+// .then(data => {
+//     console.log(data);
+//   var tempValue = data['main']['temp'];
+//   var nameValue = data['name'];
+//   var descValue = data['weather'][0]['description'];
 
-  main.innerHTML = nameValue;
-  desc.innerHTML = "Desc - "+descValue;
-  temp.innerHTML = "Temp - "+tempValue;
-  input.value ="";
-//   Math.round((data.list[i].main.temp - 273.15) * 1.8 + 32) +
-//                 " &#176;F";
+//   main.innerHTML = nameValue;
+//   desc.innerHTML = "Desc - "+descValue;
+//   temp.innerHTML = "Temp - "+tempValue;
+//   input.value ="";
+// //   Math.round((data.list[i].main.temp - 273.15) * 1.8 + 32) +
+// //                 " &#176;F";
 
-})
+// })
 
-.catch(err => alert("Wrong city name!"));
-})
+// .catch(err => alert("Wrong city name!"));
+// })
+
+// api key + variables
+const apiKey = "63606d5f87fb1ca9e2705345f9cb648e";
+var currWeatherDiv = $("#currentWeather");
+var forecastDiv = $("#weatherForecast");
+var citiesArray;
+
+// local storage for weather array
+if (localStorage.getItem("localWeatherSearches")) {
+    citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches"));
+    writeSearchHistory(citiesArray);
+} else {
+    citiesArray = [];
+};
