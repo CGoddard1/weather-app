4cc8e859d682ae3e7ef5dec65c490c1988401ca4
CGoddard1 <goddard.cole@gmail.com>
2023-03-07 17:01:11 -0500

    forecast function

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,3 +60,33 @@ function returnCurrentWeather(cityName) {
       createHistoryButton(response.name);
   })
 };
+
+// return forecast function using API
+function returnWeatherForecast(cityName) {
+  let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${apiKey}`;
+
+  $.get(queryURL).then(function(response){
+      let forecastInfo = response.list;
+      forecastDiv.empty();
+      $.each(forecastInfo, function(i) {
+          if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
+              return;
+          }
+          let forecastDate = new Date(forecastInfo[i].dt*1000);
+          let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
+
+          forecastDiv.append(`
+          <div class="col-md">
+              <div class="card text-white bg-primary">
+                  <div class="card-body">
+                      <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
+                      <img src=${weatherIcon} alt="Icon">
+                      <p>Temp: ${forecastInfo[i].main.temp} &#176;C</p>
+                      <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
+                  </div>
+              </div>
+          </div>
+          `)
+      })
+  })
+};
